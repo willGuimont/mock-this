ae55f55d8a17636ec9e2c4e6c912a126301793cf
William Guimont-Martin <wil9876543210@hotmail.com>
2021-10-12 17:59:56 -0400

    removed mongo

--- app.py
+++ app.py
@@ -2,20 +2,11 @@ import json
 import os
 import random
 
-import pymongo
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app)
-
-mongo_user = os.environ['MONGO_USER']
-mongo_pwd = os.environ['MONGO_PWD']
-
-client = pymongo.MongoClient(
-    f'mongodb+srv://{mongo_user}:{mongo_pwd}@cluster0-svqla.mongodb.net/test?retryWrites=true&w=majority')
-db = client.Cluster0
-count = db.count
 
 
 def random_case(x: str, p: float):
@@ -46,19 +37,7 @@ def mock():
     p = json_content.get('probability_upper', 0.5)
     output = {'mock': mockify(message, p=p)}
 
-    count.update({}, {"$inc": {"count": 1}})
-
     return jsonify(output)
-
-
-@app.route('/count', methods=['GET'])
-def get_count():
-    if count.find_one() is None:
-        count.insert_one({'count': 0})
-        return jsonify({'count': 0})
-
-    current_count = count.find_one()['count']
-    return jsonify({'count': current_count})
 
 
 if __name__ == '__main__':
